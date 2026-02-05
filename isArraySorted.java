95f0620f8751acd5b0de1a115d428fa4ae0d15b3
MDSaad1313 <mohammadsaadmujawar313@gmail.com>
2026-02-05 12:16:28 +0000

    isArraySorted

---
+++ isArraySorted.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class isArraySorted{
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,5,4};
+
+		boolean isSorted = true;
+
+		for(int i=0; i<arr.length-1; i++) {
+		
+			if(arr[i] > arr[i + 1]) {
+			
+				isSorted = false;
+				break;
+			}
+		}
+		System.out.println(isSorted);
+	}
+}
